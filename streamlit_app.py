fdd7afabc70eb466069e694ad273f89f44653455
PallaviVasanth <iampallavivasanth77@gmail.com>
2026-02-09 00:36:10 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from textblob import TextBlob
+import speech_recognition as sr
+
+st.title("Voice Sentiment Analyzer")
+
+audio_file = st.file_uploader("Upload voice file", type=["wav"])
+
+if audio_file:
+    r = sr.Recognizer()
+
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio)
+
+    st.write("Detected Text:", text)
+
+    sentiment = TextBlob(text).sentiment.polarity
+
+    if sentiment > 0:
+        st.success("Positive 😊")
+    elif sentiment < 0:
+        st.error("Negative 😠")
+    else:
+        st.info("Neutral 😐")
