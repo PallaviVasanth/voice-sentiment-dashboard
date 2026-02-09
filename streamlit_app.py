1796c2df9b6a2d5f3cff5d3a1d3595d1721702c2
PallaviVasanth <iampallavivasanth77@gmail.com>
2026-02-09 22:38:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,31 @@
 import streamlit as st
 from textblob import TextBlob
+import speech_recognition as sr
+import tempfile
 
 st.title("Voice Sentiment Analyzer")
 
-st.write("Enter a sentence to analyze sentiment")
+audio_file = st.file_uploader("Upload voice file", type=["wav"])
 
-text = st.text_input("Type something...")
+if audio_file is not None:
+    # Save file temporarily
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
+        temp_audio.write(audio_file.read())
+        temp_path = temp_audio.name
 
-if st.button("Analyze"):
-    if text:
-        analysis = TextBlob(text)
-        sentiment = analysis.sentiment.polarity
+    r = sr.Recognizer()
 
-        if sentiment > 0:
-            st.success("Positive")
-        elif sentiment < 0:
-            st.error("Negative")
-        else:
-            st.info("Neutral")
+    with sr.AudioFile(temp_path) as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio)
+
+    st.write("Detected Text:", text)
+
+    sentiment = TextBlob(text).sentiment.polarity
+
+    if sentiment > 0:
+        st.success("Positive")
+    elif sentiment < 0:
+        st.error("Negative")
     else:
-        st.warning("Please enter text")
-
+        st.info("Neutral")
